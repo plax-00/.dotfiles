a3f3dac34c5c452729bcff061d674e4f80695e41
plax-00 <plaxman81@gmail.com>
2024-02-15 23:19:34 -0500

    easyalign plugin and updates

--- nvim/lua/user/plugins/vim-plugins.lua
+++ nvim/lua/user/plugins/vim-plugins.lua
@@ -6,4 +6,16 @@ return {
     'jesseleite/vim-noh',
     'jrudess/vim-foldtext',
     'jeetsukumaran/vim-indentwise',
+    {
+        'junegunn/vim-easy-align',
+        init = function()
+            vim.cmd[[
+                " Start interactive EasyAlign in visual mode (e.g. vipga)
+                xmap ga <Plug>(EasyAlign)
+
+                " Start interactive EasyAlign for a motion/text object (e.g. gaip)
+                nmap ga <Plug>(EasyAlign)
+            ]]
+        end
+    }
 }

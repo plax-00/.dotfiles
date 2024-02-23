be6fc96fd7d46ba290368b067186b31087d47078
plax-00 <plaxman81@gmail.com>
2024-02-23 02:11:57 -0500

    fix enter autocompleting

--- nvim/lua/user/plugins/lsp.lua
+++ nvim/lua/user/plugins/lsp.lua
@@ -128,7 +128,7 @@ return {
                     end,
                 },
                 mapping = {
-                    ['<CR>'] = cmp.mapping.confirm { select = true },
+                    ['<CR>'] = cmp.mapping.confirm { select = false },
                     ['<C-j>'] = cmp.mapping.select_next_item(),
                     ['<C-k>'] = cmp.mapping.select_prev_item(),
 

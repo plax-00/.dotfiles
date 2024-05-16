e2a896be8f78258cf24762895c97f63c8d3c9aea
plax-00 <plaxman81@gmail.com>
2024-05-16 00:32:25 -0400

    fix dus alias

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -21,7 +21,7 @@ alias fgrep="fgrep --color=auto"
 alias grep="grep --color=auto -P"
 
 # du and df
-alias dus="du -hs * | sort -hr"
+alias dus="du -hs (*|.*) | sort -hr"
 alias dusa='du -hs $(ls -A) | sort -hr'
 alias du="du -h"
 alias df="df -h"

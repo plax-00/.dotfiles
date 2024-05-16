3c5bc09a39574949356c22e95147275b5da0fa97
plax-00 <plaxman81@gmail.com>
2024-05-16 00:38:33 -0400

    remove some old stuff

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -1,18 +1,6 @@
-local colors = {
-    '#E5E510',  --yellow
-    '#D670D6',  --purple
-    '#3B8EEA',  --blue
-    '#E5E510',  --yellow
-    '#D670D6',  --purple
-    '#3B8EEA',  --blue
-    -- '#F14C4C',  --red
-    -- '#29B8DB',  --cyan
-}
-
 return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    -- dependencies = { 'p00f/nvim-ts-rainbow' },
     opts = {
         ensure_installed = {
             'bash',

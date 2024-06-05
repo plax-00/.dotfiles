cb7847f643564153e04cadfb604b764076788998
plax-00 <plaxman81@gmail.com>
2024-06-05 00:01:00 -0400

    new dashboard header

--- nvim/lua/user/plugins/dashboard-nvim.lua
+++ nvim/lua/user/plugins/dashboard-nvim.lua
@@ -1,27 +1,3 @@
-local header = {
-    [[]],
-    [[]],
-    [[        ⢀⣴⡾⠃     ⠈⠺⠟⠛⠛⠛⠛⠻⢿⣿⣿⣿⣿⣶⣤⡀  ]],
-    [[      ⢀⣴⣿⡿⠁              ⣸⣿⣿⣿⣿⣿⣿⣿⣷ ]],
-    [[     ⣴⣿⡿⡟⡼⢹⣷⢲⡶⣖⣶⣶⢄      ⣼⣿⢿⣿⣿⣿⣿⣿⣿⣿ ]],
-    [[    ⣾⣿⡟⣾⡸⢠⡿⢳⡿⠍⣼⣿⢏⣿⣷⢄⡀ ⢠⣾⢻⣿⣸⣿⣿⣿⣿⣿⣿⣿ ]],
-    [[  ⣡⣿⣿⡟⡼⡁⠁⣰⠂⡾⠉⢨⣿⠃⣿⡿⠍⣾⣟⢤⣿⢇⣿⢇⣿⣿⢿⣿⣿⣿⣿⣿ ]],
-    [[ ⣱⣿⣿⡟⡐⣰⣧⡷⣿⣴⣧⣤⣼⣯⢸⡿⠁⣰⠟⢀⣼⠏⣲⠏⢸⣿⡟⣿⣿⣿⣿⣿⣿ ]],
-    [[ ⣿⣿⡟⠁ ⠟⣁⠄⢡⣿⣿⣿⣿⣿⣿⣦⣼⢟⢀⡼⠃⡹⠃⡀⢸⡿⢸⣿⣿⣿⣿⣿⡟ ]],
-    [[ ⣿⣿⠃ ⢀⣾⠋⠓⢰⣿⣿⣿⣿⣿⣿⠿⣿⣿⣾⣅⢔⣕⡇⡇⡼⢁⣿⣿⣿⣿⣿⣿⢣ ]],
-    [[ ⣿⡟  ⣾⣇⠷⣢⣿⣿⣿⣿⣿⣿⣿⣭⣀⡈⠙⢿⣿⣿⡇⡧⢁⣾⣿⣿⣿⣿⣿⢏⣾ ]],
-    [[ ⣿⡇ ⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢻⠇⠄⠄⢿⣿⡇⢡⣾⣿⣿⣿⣿⣿⣏⣼⣿ ]],
-    [[ ⣿⣷⢰⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⢰⣧⣀⡄⢀⠘⡿⣰⣿⣿⣿⣿⣿⣿⠟⣼⣿⣿ ]],
-    [[ ⢹⣿⢸⣿⣿⠟⠻⢿⣿⣿⣿⣿⣿⣿⣿⣶⣭⣉⣤⣿⢈⣼⣿⣿⣿⣿⣿⣿⠏⣾⣹⣿⣿ ]],
-    [[ ⢸⠇⡜⣿⡟   ⠈⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟⣱⣻⣿⣿⣿⣿⣿⠟⠁⢳⠃⣿⣿⣿ ]],
-    [[  ⣰⡗⠹⣿⣄   ⢀⣿⣿⣿⣿⣿⣿⠟⣅⣥⣿⣿⣿⣿⠿⠋  ⣾⡌⢠⣿⡿⠃ ]],
-    [[ ⠜⠋⢠⣷⢻⣿⣿⣶⣾⣿⣿⣿⣿⠿⣛⣥⣾⣿⠿⠟⠛⠉            ]],
-    [[]],
-    [[  N  E  O  V  I  M  ]],
-    [[]],
-    [[]],
-}
-
 local center = {
     {
         icon = ' ',
@@ -30,13 +6,13 @@ local center = {
         action = 'Telescope find_files hidden=true theme=dropdown prompt_title=Search\\ Directory',
     },
     {
-        icon = ' ',
+        icon = '󰈢 ',
         desc = 'Recent Files             ',
         key = 'o',
         action = 'Telescope oldfiles hidden=true theme=dropdown prompt_title=Recent\\ Files',
     },
     {
-        icon = ' ',
+        icon = '󰈞 ',
         desc = 'Grep Files               ',
         key = 'g',
         action = 'Telescope live_grep hidden=true theme=dropdown prompt_title=Grep\\ Files',
@@ -61,15 +37,38 @@ for _, v in pairs(center) do
     v.key_hl = '@string'
 end
 
+local function pad_header(header, top, bottom)
+    local top_pad = {}
+    local bottom_pad = {}
+
+    for _ = 1,top do
+        table.insert(top_pad, '')
+    end
+    for _ = 1,bottom do
+        table.insert(bottom_pad, '')
+    end
+
+    vim.list_extend(top_pad, header)
+    vim.list_extend(top_pad, bottom_pad)
+
+    return top_pad
+end
+
 
 return {
     'nvimdev/dashboard-nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {
-        theme = 'doom',
-        config = {
-            header = header,
-            center = center,
-        },
+    dependencies = {
+        'nvim-tree/nvim-web-devicons',
+        'MaximilianLloyd/ascii.nvim',
     },
+    opts = function()
+        local header = pad_header(require('ascii').art.text.neovim.sharp, 4, 8)
+        return {
+            theme = 'doom',
+            config = {
+                header = header,
+                center = center,
+            },
+        }
+    end,
 }

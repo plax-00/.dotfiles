4924500c08e25fdac96aa64623a6324eff13b9d5
plax-00 <plaxman81@gmail.com>
2024-06-07 23:09:43 -0400

    more gitsigns keymaps, now in lua

--- nvim/lua/user/plugins/git.lua
+++ nvim/lua/user/plugins/git.lua
@@ -10,10 +10,23 @@ return {
                 changedelete = { hl = 'GitSignsChange', text = '┃', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn' },
             },
         },
-        init = function()
-            vim.cmd([[
-                nnoremap    <Leader>gl      <Cmd>Gitsigns toggle_linehl<CR>
-            ]])
+        config = function(_, opts)
+            local gitsigns = require('gitsigns')
+            vim.keymap.set('n', '<Leader>gl', gitsigns.toggle_linehl)
+            vim.keymap.set('n', '<Leader>gt', gitsigns.toggle_deleted)
+            vim.keymap.set('n', '<Leader>gd', gitsigns.reset_hunk)
+            vim.keymap.set('v', '<Leader>gd', function()
+                local range_start = vim.fn.line('v')
+                local range_end = vim.fn.line('.')
+                gitsigns.reset_hunk({ range_start, range_end })
+            end)
+            vim.keymap.set('v', '<Leader>gs', function()
+                local range_start = vim.fn.line('v')
+                local range_end = vim.fn.line('.')
+                gitsigns.stage_hunk({ range_start, range_end })
+            end)
+
+            gitsigns.setup(opts)
         end,
     },
 

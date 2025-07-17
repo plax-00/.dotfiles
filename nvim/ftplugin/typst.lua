c1ff689b54e265b66b56e5a788fa0d6a862b978b
Pranav Laxman <plaxman81@gmail.com>
2025-07-16 21:39:06 -0400

    nvim: typst settings

---
+++ nvim/ftplugin/typst.lua
@@ -0,0 +1,11 @@
+vim.cmd [[
+    setlocal wrap
+    setlocal linebreak
+    setlocal spell
+    nnoremap j gj
+    nnoremap k gk
+    nnoremap A g$a
+    nnoremap I g^i
+    nnoremap <Leader>a g$
+    nnoremap <Leader>i g^
+]]

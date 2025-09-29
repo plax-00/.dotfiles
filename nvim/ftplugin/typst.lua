e29b65d2fecc20eebbc0d56c8f9fffedbfb4ce75
Pranav Laxman <plaxman81@gmail.com>
2025-09-29 11:29:11 -0400

    nvim: typst only watch main file

--- nvim/ftplugin/typst.lua
+++ nvim/ftplugin/typst.lua
@@ -9,6 +9,13 @@ vim.cmd [[
     nnoremap <Leader>a g$
     nnoremap <Leader>i g^
 ]]
+
+local fname = vim.fn.expand('%:p')
+
+if fname ~= vim.fn.fnamemodify(vim.fn.argv(0), ':p') then
+    return
+end
+
 local proc = vim.system({'typst', 'watch', vim.fn.expand('%')})
 vim.api.nvim_create_autocmd('VimLeavePre', {
    callback = function()

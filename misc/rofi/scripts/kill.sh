07ec9ae888c960136d68709892f6585a28135603
Pranav Laxman <plaxman81@gmail.com>
2025-10-20 14:38:55 -0400

    misc: add sigkill toggle to rofi kill script

--- misc/rofi/scripts/kill.sh
+++ misc/rofi/scripts/kill.sh
@@ -3,12 +3,34 @@
 set_opt() {
     echo -en "\0$1\x1f$2\n"
 }
+set_opt use-hot-keys true
+set_opt keep-selection true
+set_opt keep-filter true
 
-set_opt prompt kill
+set_sigterm() {
+    set_opt prompt sigterm
+    set_opt theme 'prompt { text-color: inherit; }'
+    set_opt data SIGTERM
+}
 
-if [[ -z "$1" ]]; then
-    ps -e -o pid,cmd --no-headers --sort=-%mem | grep -v '\d* \[.*\]'
-else
-    pid="$(echo "$1" | sed -e 's/^\s*//' | cut -d ' ' -f 1)"
-    [[ -n "$pid" ]] && (kill "$pid" || notify-send "kill $pid failed" -t 5000)
-fi
+set_sigkill() {
+    set_opt prompt sigkill
+    set_opt theme 'prompt { text-color: red; }'
+    set_opt data SIGKILL
+}
+
+proc_list() {
+    LIBPROC_HIDE_KERNEL=1 ps -e -o pid,cmd --no-headers --sort=-%mem | sed "/$$/d"
+}
+
+case "$ROFI_RETV" in
+    1)
+        pid="$(sed -e 's/^\s*//' <<< "$1" | cut -d ' ' -f 1)"
+        [[ -n "$pid" ]] \
+            && (kill -s "$ROFI_DATA" "$pid" || notify-send "$ROFI_DATA $pid failed" -t 5000)
+    ;;
+    0|10)
+        [[ "$ROFI_DATA" = "SIGTERM" ]] && set_sigkill || set_sigterm
+        proc_list
+    ;;
+esac
